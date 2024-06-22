8095d12f3a9f93e04cd1cb4e1bfc50e3b575b3ba
az1ktokhirov <azizillotokhirov@gmail.com>
2024-06-22 19:40:43 +0500

    fifth commit

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -23,28 +23,6 @@
                     <span @click="played" class="project__content-link-list-item">sun-light</span>
                 </div>
             </a>
-            <a href="https://kickss.netlify.app/" class="project__content-link kicks">
-                <div class="header__content-link-list hover-marquee ">
-                    <span @click="played" class="project__content-link-list-item default-link">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                    <span @click="played" class="project__content-link-list-item">Kicks</span>
-                </div>
-            </a>
             <a href="https://az1ktokhirov.github.io/test-project/" class="project__content-link">
                 <div class="header__content-link-list hover-marquee">
                     <span @click="played" class="project__content-link-list-item default-link">Alfa-Marketing</span>
@@ -67,6 +45,28 @@
                     <span @click="played" class="project__content-link-list-item">Alfa-Marketing</span>
                 </div>
             </a> 
+            <a href="https://kickss.netlify.app/" class="project__content-link kicks">
+                <div class="header__content-link-list hover-marquee ">
+                    <span @click="played" class="project__content-link-list-item default-link">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                    <span @click="played" class="project__content-link-list-item">Kicks</span>
+                </div>
+            </a>
         </div>
         <div class="back">
             <router-link to="/"><p class="back-icon">X</p></router-link>
