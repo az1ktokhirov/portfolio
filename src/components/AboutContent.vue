db323ead9284351b5dbed173efd5a084a497d696
az1ktokhirov <azizillotokhirov@gmail.com>
2024-07-03 13:21:15 +0500

    comit

--- src/components/AboutContent.vue
+++ src/components/AboutContent.vue
@@ -33,7 +33,7 @@
                 <h4 class="about__content-bottom-list-item"></h4>
             </div>
             <div class="about__content-bottom-resume">
-                <a href="/Azizillo-Toxirov.pdf" download="CV">
+                <a href="/public/Azizillo-Toxirov.pdf" download="CV">
                     Click to Download <br />
                     My Resume!</a
                 >
